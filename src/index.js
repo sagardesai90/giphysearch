7575f8e8df6a3da034a79584ee9ca32e0f2012d0
sagardesai90 <saagardesai@gmail.com>
2019-04-11 14:13:57 -0500

    added onChange console.log for searchBar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+    handleTermChange(search){
+        console.log(search);
+    }
+    
     render() {
         return (
             <div className="greeting">
-                <SearchBar />
+                <SearchBar onTermChange={this.handleTermChange}/>
             </div>
         );
     }
