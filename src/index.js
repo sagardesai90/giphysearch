1d02a351c3d1f0823661ac9ce89b9dff19292681
sagardesai90 <saagardesai@gmail.com>
2019-04-11 14:48:50 -0500

    added GifItem and GifList components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'https://media.giphy.com/media/XZlsb4BRzENr7tlEmN/giphy.gif'
+                },
+                {
+                    id: 2,
+                    url: 'https://media.giphy.com/media/McVXwnNOXfBmFyLkNV/giphy.gif'
+                },
+                {
+                    id: 3,
+                    url: 'https://media.giphy.com/media/LpWR8NmxOSYKDuLvJE/giphy.gif'
+                }
+            ]
+        }
+    }
+    
     handleTermChange(search){
         console.log(search);
     }
@@ -11,6 +33,7 @@ class App extends React.Component {
         return (
             <div className="greeting">
                 <SearchBar onTermChange={this.handleTermChange}/>
+                <GifList gifs={this.state.gifs} />
             </div>
         );
     }
