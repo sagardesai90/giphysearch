7383a8bf131adfe799c0e1223171c446495e9c77
sagardesai90 <saagardesai@gmail.com>
2019-04-15 11:48:49 -0500

    http -> https

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const REQUEST_GIFS = 'REQUEST_GIFS';
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const API_URL = 'https://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=d2YvdLWEdDLD9TYsnQnp6Cx03J7leWso';
 
 export function requestGifs(search = null){
